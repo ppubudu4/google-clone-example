26e877aee4149c3b5933067fd0fe0c8145e5650d
Pubudu Dananjaya <ppubudu4@gmail.com>
2021-07-24 07:31:04 +0530

    hot-fix

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,11 +1,14 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Avatar = ({ url, className }) => {
   return (
-    <img
+    <Image
       loading='lazy'
       className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
       src={url}
+      height={60}
+      width={60}
       alt='profile picture'
     />
   );
